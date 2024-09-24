cf440e3ca735d0f197cc50dba9753be1faad94c8
Syed Noor Raza <xische_sraza@dm.gov.ae>
2024-09-24 21:30:02 +0400

    chore: swagger fix and added readme for frontend

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'user123', description: 'The name for the account' })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ example: 'test@gmail.com', description: 'The email for the account' })
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
-  @MinLength(6)
+  @MinLength(8)
+  @ApiProperty({ example: '12345678', description: 'The password for the account' })
   password: string;
 }
